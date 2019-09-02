d1af5999735cb7050e2e3c24cb19bc65e0990429
a872432284 <52515527+a872432284@users.noreply.github.com>
2019-09-02 10:23:57 +0800

    1
    1

---
+++ New Unity Project/Assets/Script/GXF/fireball3.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireball3 : MonoBehaviour
+{
+    public float movespeed = 5.0f;
+    float fireball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireball = movespeed * 2.0f;
+        vector3 sudu = new vector3(0, fireball,0);
+        rigdbody.velocity = sudu;
+
+    }
+}
