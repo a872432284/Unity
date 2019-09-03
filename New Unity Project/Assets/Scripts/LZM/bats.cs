b1663e222d3ce8f6390d9be2c2c99be43bbdf199
刘泽明 <52514386+w1243063488@users.noreply.github.com>
2019-09-03 09:45:18 +0800

    1
    1

---
+++ New Unity Project/Assets/Scripts/LZM/bats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bats : MonoBehaviour
+{
+    public float speed = 50.0f;
+    float blastspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blastspeed = speed* 50.0f;
+        Vector2 sudu = new Vector2(speed,0);
+        GetComponent<Rigidbody2D>().velocity = sudu;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
